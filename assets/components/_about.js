f9654c44cb10ff4f52b5f16c6e994439fee4367a
Priyanshu <priyanshusarkar204@gmail.com>
2021-02-02 14:35:17 +0530

    Added animation

--- assets/components/_about.js
+++ assets/components/_about.js
@@ -5,19 +5,19 @@ home.innerHTML = `
         <img class="col-xl-11 col-lg-13 aos-init aos-animate" src="./assets/img/front/about-img.png">	
     </div>	
     <div class="about-content-container ml-2 d-flex">	
-        <div class="about-heading-big">	
+        <div class="about-heading-big" data-aos="fade-down">	
             <h3>Get The Best Services</h3>	
             <p>Get the best exprience with our team </p>
         </div>	
-        <div class="about-heading-small mt-4">	
+        <div class="about-heading-small mt-4" data-aos="fade-right">	
             <h4>Frontend Development</h4>	
             <p class="text-muted">We use HTML, CSS, JS and Wordpress to create dynamic and static websites ui and ux with awesome design.</p>	
         </div>	
-        <div class="about-heading-small mt-4">	
+        <div class="about-heading-small mt-4" data-aos="fade-left">	
             <h4> Backend Development </h4>	
             <p class="text-muted">We use PHP and Node.js to create dynamic and static websites' database and backend functions with security.</p>	
         </div>	
-        <div class="about-heading-small mt-4">	
+        <div class="about-heading-small mt-4" data-aos="fade-up">	
             <h4>JavaScript Framework and Libraries</h4>	
             <p class="text-muted">We use React.js, Three.js, Anime.js, Electron.js, etc to create Awesome and beautiful apps with awesome design.</p>	
         </div>	
